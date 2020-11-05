b74d003beeded2a38a339ee4e9d60c27f5cead49
nico9813 <nico.gomez.mbc@gmail.com>
2020-11-05 16:05:06 -0300

    add searchbar to filter tareas in a board

--- prello/components/ListasEstados.js
+++ prello/components/ListasEstados.js
@@ -11,6 +11,7 @@ export const ListasEstados = (props) => {
   const { id } = Tablero
   const fetchPrelloApi = useFetchPrelloApi()
   const dispatch = useDispatch()
+  const [filtrosActivos, setFiltrosActivos] = useState([])
 
   const [ isOpen, setIsOpen ] = useState(false)
 
@@ -20,6 +21,20 @@ export const ListasEstados = (props) => {
     dispatch(crear_agregar_estado(id, estado))
   }
 
+  const aplicarFiltro = ([key, expectedValue], tarea) => tarea[key] == expectedValue
+
+  const parseFiltro = (filtro) => {
+    return filtro.split('=')
+  }
+
+  const agregarFiltro = (evt) => {
+    const filtro = parseFiltro(evt.target.value)
+    console.log(filtro)
+    if (filtro.length  > 1){
+      setFiltrosActivos(prevState => [...prevState, filtro])
+    }
+  }
+
   return (
     <>
       <div className={styles.titulo}>
@@ -27,12 +42,16 @@ export const ListasEstados = (props) => {
         <h4 onClick={ () => setIsOpen(true)}>Add</h4>
         {isOpen && <EstadoModal isOpen={isOpen} onClose={agregarEstado}/>}
       </div>
+      <input placeholder="Agregar filtro" className={styles.searchBar} onKeyDown={(evt) => {if(evt.key === 'Enter') agregarFiltro(evt)}}/>
+      <div>
+        {filtrosActivos.map( (value, index) => <p onClick={() => setFiltrosActivos(prevState => prevState.filter((_, indexActual) => indexActual != index))}>{JSON.stringify(value)}</p>)}
+      </div>
       <div className={styles.container}>
         {Estados.map((estado, index) => (
           <Estado
             key={index}
             Estado={estado}
-            Tareas={Tareas.filter((tarea) => tarea.estado.id == estado.id)}
+            Tareas={Tareas.filter((tarea) => tarea.estado.id == estado.id && filtrosActivos.every(filtro => aplicarFiltro(filtro, tarea)))}
             Tablero={Tablero}
           />
         ))}
