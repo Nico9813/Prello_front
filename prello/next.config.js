833b0aeb82591bae812f09e4ba45c124799ae4de
nico9813 <nico.gomez.mbc@gmail.com>
2020-12-21 15:19:15 -0300

    fix module css

--- prello/next.config.js
+++ prello/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {	
   env: {	
     HOST: process.env.NODE_ENV == 'development'? 'http://localhost:3000/' : process.env.HOST,	
-    API: process.env.NODE_ENV == 'development'? 'http://127.0.0.1:59794/' : process.env.API,	
+    API: process.env.NODE_ENV == 'development'? 'http://127.0.0.1:51451/' : process.env.API,	
     REALTIME_HOST: process.env.NODE_ENV == 'development'? 'http://localhost:8000/' : process.env.REALTIME_HOST,	
   },	
 } 
